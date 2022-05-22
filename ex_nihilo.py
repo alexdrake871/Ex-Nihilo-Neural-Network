ef69f29e4b1b06f2fd9e4f478ead69295d739faf
Alexander Drake <alexdrake871@gmail.com>
2022-05-21 19:31:08 -0500

    softmax activation function

--- ex_nihilo.py
+++ ex_nihilo.py
@@ -7,15 +7,13 @@ from dataset import sin_data_set
 
 class XN_Neural_Network():
     '''The neural network which will be interacted with directly.'''
-    def __init__(self, input_layer_size: int, output_layer_size: int, num_hidden_layers: int, nodes_per_hidden_layer: int):
+    def __init__(self, input_layer_size: int, num_hidden_layers: int, nodes_per_hidden_layer: int):
         # Store number of inputs
         self.input_layer_size = input_layer_size
 
-        # Create output layer
-        if num_hidden_layers > 0:
-            self.output_layer = Layer(output_layer_size, nodes_per_hidden_layer)
-        else:
-            self.output_layer = Layer(output_layer_size, input_layer_size)
+        # Store other parameters
+        self.num_hidden_layers = num_hidden_layers
+        self.nodes_per_hidden_layer = nodes_per_hidden_layer
 
         # Create hidden layers
         self.hidden_layers = []
@@ -27,7 +25,23 @@ class XN_Neural_Network():
                 new_hidden_layer = Layer(nodes_per_hidden_layer, nodes_per_hidden_layer)
             self.hidden_layers.append(new_hidden_layer)
     
-    def train(self, X, Y, ):
+    def train(self, X, y):
+        # Determine how many outputs are needed
+        self.output_labels = []
+        for output in y:
+            if output not in self.output_labels:
+                self.output_labels.append(output)
+        self.output_layer_size = len(self.output_labels)
+
+        # Create output layer based on y-values
+        if self.num_hidden_layers > 0:
+            self.output_layer = Layer(self.output_layer_size, self.nodes_per_hidden_layer, True)
+        else:
+            self.output_layer = Layer(self.output_layer_size, self.input_layer_size, True)
+    
+    # FIXME
+    def display(self):
+        '''Display a visual representation of the trained neural network.'''
         pass
 
     def classify(self, input_vector: list[float]) -> list[float]:
@@ -44,10 +58,11 @@ class XN_Neural_Network():
 
 class Layer():
     '''Hidden layer of neural network.'''
-    def __init__(self, num_nodes: int, num_inputs: int):
+    def __init__(self, num_nodes: int, num_inputs: int, output_layer = False):
+        self.output_layer = output_layer    # Flag showing whether its output layer or hidden layer
         self.nodes = []
         for i in range(num_nodes):
-            new_node = Node(num_inputs)
+            new_node = Node(num_inputs, self.output_layer)
             self.nodes.append(new_node)
     
     def get_node_outputs(self, inputs: list[float]) -> list[float]:
@@ -55,20 +70,33 @@ class Layer():
         for node in self.nodes:
             node_output = node.get_output(inputs)
             outputs.append(node_output)
-        return outputs
+        
+        if not self.output_layer:   # Return the hidden layer's outputs
+            return outputs
+        else:                       # Return the proportional representation of output layer's outputs
+            output_sum = sum(outputs)
+            proportional_values = []
+            for val in outputs:
+                proportional_values.append(val / output_sum)
+            return proportional_values
 
 
 class Node():
     '''A single node/neuron within the layers of the neural network.'''
-    def __init__(self, num_inputs: int):
+    def __init__(self, num_inputs: int, output_node = False):
         # FIXME OR DON'T: Can use the numpy zeroes function here
         self.weights = []
         self.bias = 0.0
+        self.output_node = output_node  # Flag showing whether node is part of output layer or hidden layer
         for i in range(num_inputs):
             self.weights.append(random.uniform(-0.1, 0.1))
     
-    def relu(self, x: float):
+    def ReLU(self, x: float):
         return max(0.0, x)
+    
+    def softmax(self, x: float):
+        E = math.e
+        return E**x
     
     def get_output(self, inputs: list[float]) -> float:
         # # FIXME OR DON'T: With numpy can simplify this to:
@@ -80,7 +108,12 @@ class Node():
             output += self.weights[idx] * val
         
         output += self.bias
-        return self.relu(output)
+
+        # Output node uses softmax activation function while hidden layer uses ReLU
+        if self.output_node:
+            return self.softmax(output)
+        else:
+            return self.ReLU(output)
 
 
 
@@ -89,4 +122,7 @@ class Node():
 the_X, the_y = sin_data_set(1000)
 features = len(the_X[0])
 
-the_network = XN_Neural_Network()
+the_network = XN_Neural_Network(features, 3, 8)
+the_network.train(the_X, the_y)
+print(the_network.classify(the_X[4]))
+print(the_network.output_layer_size)
